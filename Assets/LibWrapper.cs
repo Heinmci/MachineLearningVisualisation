0d28fab50a9fe4304c7d967fa073cdcdc1ef0ed2
Benoit <benoit.chopinet@sfr.fr>
2018-03-05 16:36:22 +0100

    initial commit

---
+++ Assets/LibWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class LibWrapper {
+	[DllImport("unity_rust")]
+	public static extern int random_classification(float y);
+}
---
+++ Assets/MoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	[SerializeField]
+	private Transform[] sphereTransforms;
+	// Use this for initialization
+	void Start () {
+		foreach (var sphere in sphereTransforms){
+			int result = LibWrapper.random_classification(sphere.position.z);
+			if(result <0) {
+				sphere.position += Vector3.down;
+			} else
+			{
+				sphere.position += Vector3.up;
+			}
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
