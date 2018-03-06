27b2af68317b528206c1f4e37824abc1396a6b77
Heinmci <dejardin@hotmail.co.uk>
2018-03-06 19:06:26 +0100

    ...

--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -13,9 +13,15 @@ public class MoveScript : MonoBehaviour {
 		Debug.Log("azeaz");
 		System.IntPtr coeff = LibWrapper.linear_create();
 		double[] points = {trainningExample[0].position.x,trainningExample[0].position.z,-1,trainningExample[1].position.x,trainningExample[1].position.z,-1,trainningExample[2].position.x,trainningExample[2].position.z,1};
-		Debug.Log(LibWrapper.get_weight(coeff, 0));
-		Debug.Log(LibWrapper.get_weight(coeff, 1));
-		Debug.Log(LibWrapper.get_weight(coeff, 2));
+		Debug.Log(points[0]);
+		Debug.Log(points[1]);
+		Debug.Log(points[2]);
+		Debug.Log(points[3]);
+		Debug.Log(points[4]);
+		Debug.Log(points[5]);
+		Debug.Log(points[6]);
+		Debug.Log(points[7]);
+		Debug.Log(points[8]);
 		LibWrapper.linear_train_classification(coeff,points);
 		Debug.Log(LibWrapper.get_weight(coeff, 0));
 		Debug.Log(LibWrapper.get_weight(coeff, 1));
