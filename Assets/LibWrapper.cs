3527730deddc511c75b72ade461a053996898db2
Benoit <benoit.chopinet@sfr.fr>
2018-03-06 16:50:41 +0100

    bla

--- Assets/LibWrapper.cs
+++ Assets/LibWrapper.cs
@@ -6,4 +6,13 @@ using System.Runtime.InteropServices;
 public class LibWrapper {
 	[DllImport("unity_rust")]
 	public static extern int random_classification(float y);
+
+	[DllImport("unity_rust")]
+	public static extern System.IntPtr linear_create();
+
+	[DllImport("unity_rust")]
+	public static extern void linear_train_classification(System.IntPtr coeff, double[] points);
+
+	[DllImport("unity_rust")]
+	public static extern int classify_point(System.IntPtr coeff, double[] point);
 }
--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -6,10 +6,17 @@ public class MoveScript : MonoBehaviour {
 
 	[SerializeField]
 	private Transform[] sphereTransforms;
+	[SerializeField]
+	private Transform[] trainningExample;
 	// Use this for initialization
 	void Start () {
+		System.IntPtr  coeff = LibWrapper.linear_create();
+		double[] points = {trainningExample[0].position.x,trainningExample[0].position.z,-1,trainningExample[0].position.x,trainningExample[0].position.z,-1,trainningExample[0].position.x,trainningExample[0].position.z,1};
+		
+		LibWrapper.linear_train_classification(coeff,points);
 		foreach (var sphere in sphereTransforms){
-			int result = LibWrapper.random_classification(sphere.position.z);
+			double[] point = {sphere.position.x,sphere.position.z};
+			int result = LibWrapper.classify_point(coeff,point);
 			if(result <0) {
 				sphere.position += Vector3.down;
 			} else
@@ -18,6 +25,7 @@ public class MoveScript : MonoBehaviour {
 			}
 			
 		}
+		
 	}
 	
 	// Update is called once per frame
